395cafe3e373fef09009da3f8d0ab9f173fa675e
evrlast <evrlast@users.noreply.github.com>
2020-04-28 23:09:31 +0300

    debug commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,25 +6,18 @@ import { AppComponent } from './app.component';
 import { DotComponent } from './components/dot/dot.component';
 import { ToolBarComponent } from './components/tool-bar/tool-bar.component';
 import { ColorPickerModule } from 'ngx-color-picker';
-import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
-import { DocumentListComponent } from './document-list/document-list.component';
-import { DocumentComponent } from './document/document.component';
 
-const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
 
 @NgModule({
   declarations: [
     AppComponent,
     DotComponent,
     ToolBarComponent,
-    DocumentListComponent,
-    DocumentComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ColorPickerModule,
-    SocketIoModule.forRoot(config),
   ],
   providers: [],
   bootstrap: [AppComponent]
